50807a565bc0b8475842879a3aa59b351e8e2f83
amesy <amesy1110@gmail.com>
2019-12-02 23:03:44 +0800

    the first commit

---
+++ yangbin/week01/main.go
@@ -0,0 +1,32 @@
+
+/*
+	例如有8支球队，两两组合的选择有多少种？ (8-1)*(8-3)*(8-5)*(8-7)=105
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func Choose(n int) (res int) {
+	res = 1
+	for i:=1; i < n; i++ {
+		if i % 2 != 0 {
+			res *= (n-i)
+		}
+	}
+
+	return
+}
+
+func main() {
+	n := 8
+	if n % 2 != 0 {
+		res := "n 不是偶数，不符合规范。"
+		fmt.Println(res)
+		return
+	}
+
+	fmt.Println(Choose(n))
+}
