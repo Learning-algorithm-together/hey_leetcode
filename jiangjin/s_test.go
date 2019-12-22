5d8e1708315e9b9bf834d4e4fc67be60286c75a9
饶江金 <raojiangjin@gmail.com>
2019-12-22 21:54:30 +0800

    feat(map)#

--- jiangjin/s_test.go
+++ jiangjin/s_test.go
@@ -1,105 +1,48 @@
 package jiangjin
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 )
 
 func TestISVaild(t *testing.T){
+	fmt.Println(isAnagram("a", "b"))
 
 }
 
-
-type MyStack struct {
-	flag int
-	data []Queue
-}
-
-
-/** Initialize your data structure here. */
-func Constructor() MyStack {
-	return MyStack{
-		flag:0,
-		data:[]Queue{
-			NewQueue(),
-			NewQueue(),
-		},
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t){
+		return false
 	}
-}
-
-
-/** Push element x onto stack. */
-func (this *MyStack) Push(x int)  {
-	this.data[this.flag].Push(x)
-}
-
-
-/** Removes the element on top of the stack and returns that element. */
-func (this *MyStack) Pop() int {
-	if this.data[this.flag].Len() == 1{
-		return this.data[this.flag].Pop()
+	m := make(map[uint8]int)
+	for i:=0;i<len(s);i++{
+		m[s[i]]++
+		m[t[i]]--
 	}
-	for this.data[this.flag].Len() != 1{
-		item := this.data[this.flag].Pop()
-		if this.flag == 1{
-			this.data[0].Push(item)
-		}else{
-			this.data[1].Push(item)
+
+	for _, v:= range m{
+		if v != 0{
+			return false
 		}
 	}
 
-	// 切换队列
-	item := this.data[this.flag].Pop()
-	if this.flag == 1{
-		this.flag = 0
-	}else{
-		this.flag = 1
+	return true
+}
+
+func StringToRuneSlice(s string) []rune {
+	var r []rune
+	for _, runeValue := range s {
+		r = append(r, runeValue)
 	}
-	return item
+	return r
 }
 
-
-/** Get the top element. */
-func (this *MyStack) Top() int {
-	x := this.Pop()
-	this.Push(x)
-	return x
+func SortStringByCharacter(s string) string {
+	r := StringToRuneSlice(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
 }
 
-
-/** Returns whether the stack is empty. */
-func (this *MyStack) Empty() bool {
-	return len(this.data[this.flag].data) == 0
-}
-
-type Queue struct{
-	data []int
-}
-
-func NewQueue() Queue{
-	return Queue{data:[]int{}}
-}
-
-func (q *Queue) Push(x int){
-	q.data = append(q.data, x)
-}
-
-func (q *Queue) Pop() int {
-	if q.IsEmpty(){
-		return 0
-	}
-	x := q.data[0]
-	q.data = q.data[1:]
-	return x
-}
-
-func (q *Queue) Peek() int {
-	return q.data[0]
-}
-
-func (q *Queue) Len() int{
-	return len(q.data)
-}
-
-func (q *Queue) IsEmpty() bool {
-	return q.Len() == 0
-}
